65ea3e8a487904ef8aa5688949353fc9cbb80f6e
zhengdunhao <550260496@qq.com>
2016-11-17 19:11:52 +0800

    test

---
+++ demo-basic/src/main/java/com/test/login/controller/UserLoginController.java
@@ -0,0 +1,20 @@
+package com.test.login.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/userLoginController")
+public class UserLoginController {
+
+	@RequestMapping(value="/user/forwardUserLogin")
+	public ModelAndView forwardUserLogin(HttpServletRequest request,HttpServletResponse response){
+		System.out.println("forwardUserLogin");
+		ModelAndView mv = new ModelAndView("login/login");
+		return mv;
+	}
+}
